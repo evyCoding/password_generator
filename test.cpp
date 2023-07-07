c388d0f36e172cf1285852a44a5985a94de176ba
yahya chami <123872563+dzap00@users.noreply.github.com>
2023-07-07 13:53:50 +0100

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,19 +1,40 @@
-//no changes for now but it will be some in the future if God wills :)
 #include <iostream>
 #include <cstdlib>
-#include <chrono>
 #include <ctime>
-int main(){
-    const char alpha[]= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const char sign[6]={'!', '?', '*', '-', '+', '&'};
-    std::srand(std::chrono::high_resolution_clock::now().time_since_epoch().count());
-    std::string password;
-     for(int i=0; i<3; i++){
-        password += alpha[rand()%26];
-        password += std::to_string(rand()%1000);
-        password += sign[rand()%6];
 
-     }
-       std::cout<<password;
-    return 0;}
+std::string password_generator(int length, bool numbers, bool specialCaracters, bool uppercase, bool lowercase)
+{
+   std::string password;
+   std::string uppercases = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+   std::string lowercases = "abcdefghijklmnopqrstuvwxyz";
+   std::string number = "0123456789";
+   std::string symboles = "/*-%.?!:$^~&)'_@)=";
+   std::string all_int_all("");
+   if(numbers == true)
+      all_int_all += number;
+   if(specialCaracters == true)
+      all_int_all += symboles;
+   if(uppercase == true)
+      all_int_all += uppercases;
+   if(lowercase == true)
+      all_int_all += lowercases;
+   srand(time(0));
+   while(password.length() != length)
+      password += all_int_all[rand() % (all_int_all.length() - 1)];
+   return password;
+}
 
+int main ()
+{
+   int length;
+   std::cin >> length;
+   bool one;
+   std::cin >> one;
+   bool two;
+   std::cin >> two;
+   bool three;
+   std::cin >> three;
+   bool four;
+   std::cin >> four;
+   std::cout << password_generator(length, one, two, three, four);
+}
